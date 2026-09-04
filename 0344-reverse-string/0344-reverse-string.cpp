1f5ed9b9fa5ee5fef74978f102b266b04ae1822d
Ansh Pandey <anshp1161@gmail.com>
2026-09-04 11:36:51 +0530

    Time: 0 ms (100%), Space: 27.8 MB (5.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -6,7 +6,6 @@ public:
         return rev(s,i+1,j-1);
     }
     void reverseString(vector<char>& s) {
-    vector<char> ans=rev(s,0,s.size()-1);
-    for(int i=0;i<ans.size();i++) cout<<ans[i];
+    s=rev(s,0,s.size()-1);
     }
 };
